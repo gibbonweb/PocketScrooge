8239fd0d4009b9e479313737449ff5127b1ca9b6
Johannes Becker <jo.becker@gmail.com>
2012-06-04 17:08:36 +0200

    remove incompatible stuff for now

--- themes/default/default.tpl.php
+++ themes/default/default.tpl.php
@@ -2,29 +2,6 @@
 
 {% block headStuff %}
 	{% parentblock %}
-		<script type="text/javascript">
-			/*google.load("visualization", "1", {packages:["corechart"]});
-			google.setOnLoadCallback(drawChart);
-			function drawChart() {
-				var data = new google.visualization.DataTable();
-				data.addColumn('date','Tag');
-				data.addColumn('number','Off');
-				data.addColumn('number','Deff');
-				
-				{% for units in user.unitreports %}
-		data.addRow({% autoescape off %}[new Date({{units.mtime}}), {{units.off}}, {{units.deff}}]{% endautoescape %});
-
-				{% endfor %}
-				var options = {
-					isStacked: true,
-					legend: 'bottom'		
-				};
-
-			
-				var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-				chart.draw(data, options);
-			}*/
-		</script>
 {% endblock %}
 
 {% block pageTitle %}PocketScrooge{% endblock %}
